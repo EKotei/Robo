244c335cc9f3ce599d9e8d23908333c38946da9c
EKotei <61988608+EKotei@users.noreply.github.com>
2020-04-27 15:39:13 -0400

    Update Robo.java

--- src/Battle/Robo.java
+++ src/Battle/Robo.java
@@ -1,9 +1,7 @@
 package Battle;
 import robocode.ScannedRobotEvent;
 import robocode.Robocode;
-
 import java.awt.Color;
-
 import robocode.*;
 
 //Robo uses AdvancedRobot methods
@@ -17,38 +15,27 @@ public class Robo extends AdvancedRobot {
 		setScanColor(Color.pink);
 		setBulletColor(Color.pink);
 		
-			
-		
-		
 		//movement pattern for robot
-		while (true) {	
-			 					
-					 
-
+	while (true) {	
+			 				
+		ahead(100);
+		turnGunRight(360);
+		scan();
+		back(75);
+		ahead(150);
+		back(150);
+		turnRight(90);
+		turnGunRight(360);
+		scan();
+		back(150);
+		turnRight(90);
+		setTurnRadarLeft(360);
+		turnGunRight(360);
+		scan();
 					
-					ahead(100);
-					turnGunRight(360);
-					scan();
-					back(75);
-					ahead(150);
-					back(150);
-					turnRight(90);
-					turnGunRight(360);
-					scan();
-					back(150);
-					turnRight(90);
-					setTurnRadarLeft(360);
-					turnGunRight(360);
-					scan();
-					
-			
 	}
-			
-	
-					
-		
+				
 }
-
 	
 //fires a bullet when enemy robot is scanned
 	 public void onScannedRobot(ScannedRobotEvent e) {
@@ -63,8 +50,7 @@ public class Robo extends AdvancedRobot {
 		 setTurnRadarRightRadians(getRadarTurnRemainingRadians());
 		
 	    }
-	
-		 
+			 
 	public void onHitByBullet(HitByBulletEvent e){
 		turnRight(45);
 		back(100);
@@ -72,12 +58,12 @@ public class Robo extends AdvancedRobot {
 		ahead(50);
 		}							
 	
-
+}
 		
 		
 		
 		
-	}
+	
 		
 
 
