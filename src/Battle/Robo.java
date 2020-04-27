46d009e776949d1e899036e6d0b9a04fe368b0fb
EKotei <61988608+EKotei@users.noreply.github.com>
2020-04-27 10:06:29 -0400

    Update Robo.java

--- src/Battle/Robo.java
+++ src/Battle/Robo.java
@@ -6,44 +6,78 @@ import java.awt.Color;
 
 import robocode.*;
 
-public class Robo extends Robot {
+//Robo uses AdvancedRobot methods
+public class Robo extends AdvancedRobot {
 	
+	//sets colors for robot
 	public void run() {
 		setBodyColor(Color.pink);
 		setGunColor(Color.black);
 		setRadarColor(Color.pink);
 		setScanColor(Color.pink);
 		setBulletColor(Color.pink);
+		
 			
-		setAdjustGunForRobotTurn(true);
-			
-		while (true) {
-					ahead(100);
-					turnGunRight(360);
-					back(75);
-					turnGunRight(360);
-					ahead(50);
-					turnGunRight(180);
-					ahead(150);
-					turnGunRight(180);
-					back(150);
-					turnGunRight(180);					
-					turnLeft(90);
-					ahead(100);
-	}	
-			 
-				 
-			
-	}		
+		
+		
+		//movement pattern for robot
+		while (true) {	
+			 					
+					 
 
+					
+					ahead(100);
+					turnGunRight(360);
+					scan();
+					back(75);
+					ahead(150);
+					back(150);
+					turnRight(90);
+					turnGunRight(360);
+					scan();
+					back(150);
+					turnRight(90);
+					setTurnRadarLeft(360);
+					turnGunRight(360);
+					scan();
+					
+			
+	}
+			
+	
+					
+		
+}
+
+	
+//fires a bullet when enemy robot is scanned
 	 public void onScannedRobot(ScannedRobotEvent e) {
-	        fire(3);
+		 e.getBearingRadians(); 
+		  if (getEnergy() > 50) {
+			fire(3.0);  
+		  }
+		  else
+		  {
+			  fire(1.0);
+		  }
+		 setTurnRadarRightRadians(getRadarTurnRemainingRadians());
+		
 	    }
 	
 		 
-	 
+	public void onHitByBullet(HitByBulletEvent e){
+		turnRight(45);
+		back(100);
+		turnRight(45);
+		ahead(50);
+		}							
+	
 
-}		
-
+		
+		
+		
+		
+	}
+		
 
 
